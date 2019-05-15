d54be2b05426db364dabe8b7d6d885a59ea56243
Bernard <bernard@ducourier.com>
2019-05-15 13:56:10 -0400

    Update validation

--- test/readme.md
+++ test/readme.md
@@ -1,7 +1,11 @@
 # Manually running validation
 
-Manual execution of validation can be done like this:
+To manual validate the template:
 
-Commit updates to dev branch then run:
+1. Login to validation subscription
+2. Commit changes to the dev branch
+3. Run the following command:
 
+```powershell
 .\validate.ps1
+```
--- test/validate.ps1
+++ test/validate.ps1
@@ -1,5 +1,5 @@
 Param(
-    [Parameter(Mandatory = $false)][string]$templateLibraryName = "asg",
+    [Parameter(Mandatory = $false)][string]$templateLibraryName = "availabilityset",
     [string]$templateName = "azuredeploy.json",
     [string]$Location = "canadacentral",
     [string]$subscription = "2de839a0-37f9-4163-a32a-e1bdb8d6eb7e"
@@ -10,16 +10,39 @@ Param(
 # Execution begins here
 #******************************************************************************
 
+function getValidationURL {
+    $remoteURL = git config --get remote.origin.url
+    $currentBranch = git rev-parse --abbrev-ref HEAD
+    $remoteURLnogit = $remoteURL -replace '\.git', ''
+    $remoteURLRAW = $remoteURLnogit -replace 'github.com', 'raw.githubusercontent.com'
+    $validateURL = $remoteURLRAW + '/' + $currentBranch + '/template/azuredeploy.json'
+    return $validateURL
+}
+
+$currentBranch = git rev-parse --abbrev-ref HEAD
+
+if ($currentBranch -eq 'master') {
+    $confirmation = Read-Host "You are working off the master branch... are you sure you want to validate the template from here? Switch to the dev branch is recommended. Continue? (y/n)"
+    if (!$confirmation -eq 'y') {
+        exit
+    }
+}
+
+# Make sure we update code to git
+# git branch dev ; git checkout dev ; git pull origin dev
+git add . ; git commit -m "Update validation" ; git push origin $currentBranch
+
 Select-AzureRmSubscription -Subscription $subscription
 
 # Cleanup validation resource content in case it did not properly completed and left over components are still lingeringcd
-Write-Host "Cleanup validation resource content...";
-New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-availabilityset-RG -Mode Complete -TemplateFile (Resolve-Path "$PSScriptRoot\parameters\cleanup.json") -Force -Verbose
+Write-Host "Cleanup old $templateLibraryName validation resources if needed...";
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-$templateLibraryName-RG -Mode Complete -TemplateFile (Resolve-Path "$PSScriptRoot\parameters\cleanup.json") -Force -Verbose
 
 # Start the deployment
-Write-Host "Starting validation deployment...";
+Write-Host "Starting $templateLibraryName dependancies deployment...";
 
-New-AzureRmDeployment -Location $Location -Name "Deploy-Infrastructure-Dependancies" -TemplateUri "https://raw.githubusercontent.com/canada-ca/accelerators_accelerateurs-azure/master/Templates/arm/masterdeploy/20190319.1/masterdeploysub.json" -TemplateParameterFile (Resolve-Path -Path "$PSScriptRoot\parameters\masterdeploysub.parameters.json") -Verbose;
+New-AzureRmDeployment -Location $Location -Name "Deploy-Infrastructure-Dependancies" -TemplateUri "https://raw.githubusercontent.com/canada-ca-azure-templates/masterdeploy/20190514/template/masterdeploysub.json" -TemplateParameterFile (Resolve-Path -Path "$PSScriptRoot\parameters\masterdeploysub.parameters.json") -Verbose;
 
 $provisionningState = (Get-AzureRmDeployment -Name "Deploy-Infrastructure-Dependancies").ProvisioningState
 
@@ -28,6 +51,18 @@ if ($provisionningState -eq "Failed") {
     exit
 }
 
+# Validating server template
+Write-Host "Starting $templateLibraryName validation deployment...";
+
+$validationURL = getValidationURL
+New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-$templateLibraryName-RG -Name "validate-$templateLibraryName-template" -TemplateUri $validationURL -TemplateParameterFile (Resolve-Path "$PSScriptRoot\parameters\validate.parameters.json") -Verbose
+
+$provisionningState = (Get-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-$templateLibraryName-RG -Name "validate-$templateLibraryName-template").ProvisioningState
+
+if ($provisionningState -eq "Failed") {
+    Write-Host  "Test deployment failed..."
+}
+
 # Cleanup validation resource content
-Write-Host "Cleanup validation resource content...";
-New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-availabilityset-RG -Mode Complete -TemplateFile (Resolve-Path "$PSScriptRoot\parameters\cleanup.json") -Force -Verbose
+Write-Host "Cleanup $templateLibraryName validation resources...";
+New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-$templateLibraryName-RG -Mode Complete -TemplateFile (Resolve-Path "$PSScriptRoot\parameters\cleanup.json") -Force -Verbose
